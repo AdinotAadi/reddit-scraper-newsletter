60fda85eade383a61157ce348a9f353ebfa3cd78
Aditya Patel <patel.aditya1@outlook.com>
2022-01-31 13:39:05 +0530

    Optimizing multithreading.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Importing the required modules.
 from msilib.schema import Class
+from multiprocessing.connection import wait
 import praw
 import time
 import threading
@@ -16,42 +17,39 @@ i = 5
 
 class fetchArticle:
     def fetchHotArticle(self):
-        letterText.append("*** Hot Submissions!!! ***\n")
+        letterText.append(f"*** Your curation of {i} of the most Hot, Rising, New and Controversial Posts on your favorite Subreddit!!! ***\n")
         for hotSubmission in reddit.subreddit(f"{sub}").hot(limit=i):
             letterText.append(hotSubmission.title + " " + "(" + hotSubmission.url + ")\n")
 
     def fetchRisingArticle(self):
-        letterText.append("*** Rising Submissions!!! ***\n")
         for risingSubmission in reddit.subreddit(f"{sub}").rising(limit=i):
             letterText.append(risingSubmission.title + " " + "(" + risingSubmission.url + ")\n")
 
     def fetchNewArticle(self):
-        letterText.append("*** New Submissions!!! ***\n")
         for newSubmission in reddit.subreddit(f"{sub}").new(limit=i):
             letterText.append(newSubmission.title + " " + "(" + newSubmission.url + ")\n")
 
     def fetchConArticle(self):
-        letterText.append("*** Controversial Submissions!!! ***\n")
         for conSubmission in reddit.subreddit(f"{sub}").controversial(limit=i):
             letterText.append(conSubmission.title + " " + "(" + conSubmission.url + ")\n")
 
     def __init__(self):
         t1 = threading.Thread(target=self.fetchHotArticle)
-        t2 = threading.Thread(target=self.fetchRisingArticle)
-        t3 = threading.Thread(target=self.fetchNewArticle)
-        t4 = threading.Thread(target=self.fetchConArticle)
-
         t1.start()
-        t1.join()
 
+        t2 = threading.Thread(target=self.fetchRisingArticle)
         t2.start()
-        t2.join()
 
+        t3 = threading.Thread(target=self.fetchNewArticle)
         t3.start()
-        t3.join()
 
+        t4 = threading.Thread(target=self.fetchConArticle)
         t4.start()
-        t4.join() 
+        
+        t1.join()
+        t2.join()
+        t3.join()
+        t4.join()
         
 def writeToFile(i, j, file):
     while j < i:
@@ -69,7 +67,7 @@ if __name__ == "__main__":
 
     file = open("Test.txt", "w+")
 
-    sub = "piracy"
+    sub = "NuclearRevenge"
 
     # subName = sub.display_name
     subTitle = sub.title
